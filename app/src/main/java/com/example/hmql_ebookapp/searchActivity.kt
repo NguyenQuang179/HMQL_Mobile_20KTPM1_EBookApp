1d22dc3b591bf28848e03e0a90685e59a194f341
tuanlam1610 <73232399+tuanlam1610@users.noreply.github.com>
2023-03-27 21:48:40 +0700

    init Search Activity

---
+++ app/src/main/java/com/example/hmql_ebookapp/searchActivity.kt
@@ -0,0 +1,45 @@
+package com.example.hmql_ebookapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+
+class searchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        var listOfTypeSearch =  resources.getStringArray(R.array.SearchType)
+        var spinner = findViewById<Spinner>(R.id.searchTypeSpinner);
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_dropdown_item, listOfTypeSearch
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    Toast.makeText(
+                        this@searchActivity, spinner.selectedItem.toString(), Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+    }
+}
