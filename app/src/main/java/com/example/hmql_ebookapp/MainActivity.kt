149e94818eb28ed9be87ca00f02db49abe641d23
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2023-03-28 15:28:43 +0700

    Init Reading Screen, Update Font

--- app/src/main/java/com/example/hmql_ebookapp/MainActivity.kt
+++ app/src/main/java/com/example/hmql_ebookapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hmql_ebookapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent : Intent = Intent(this, ReadingScreen::class.java)
+        startActivity(intent)
     }
 }
