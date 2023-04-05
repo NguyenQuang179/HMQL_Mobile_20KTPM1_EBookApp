f33dbb75a5004eeef3a77afcc61c1f0189b6cea0
Hoang Minh <hoangminhbadao@gmail.com>
2023-04-05 23:05:08 +0700

    add new Book Info fragment kotlin class file

--- app/src/main/java/com/example/hmql_ebookapp/BookIntroductionActivity.kt
+++ app/src/main/java/com/example/hmql_ebookapp/BookIntroductionActivity.kt
@@ -3,33 +3,38 @@ package com.example.hmql_ebookapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.FragmentContainerView
 
 class BookIntroductionActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_introduction)
 
+
+
         // calling the action bar
-        var actionBar = getSupportActionBar()
-
-        if (actionBar != null) {
-
-            // Customize the back button
-            actionBar.setHomeAsUpIndicator(R.drawable.back_button);
-
-            // showing the back button in action bar
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+//        var actionBar = getSupportActionBar()
+//
+//        if (actionBar != null) {
+//
+//            // Customize the back button
+//            actionBar.setHomeAsUpIndicator(R.drawable.back_button);
+//
+//            // showing the back button in action bar
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
     }
     // this event will enable the back
     // function to the button on press
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-        }
-        return super.onContextItemSelected(item)
-    }
+//    override fun onContextItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
+//            }
+//        }
+//        return super.onContextItemSelected(item)
+//    }
 }
---
+++ app/src/main/java/com/example/hmql_ebookapp/BookIntroduction_InfoFragment.kt
@@ -0,0 +1,6 @@
+package com.example.hmql_ebookapp
+
+import androidx.fragment.app.Fragment
+
+class BookIntroduction_InfoFragment : Fragment(R.layout.fragment_book_intro){
+}
