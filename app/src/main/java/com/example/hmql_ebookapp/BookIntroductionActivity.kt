90b922d52bebf376faa8f7dc3688075da97bae73
Hoang Minh <hoangminhbadao@gmail.com>
2023-04-20 19:59:44 +0700

    Fix bottom nav bar Book Intro not showing

--- app/src/main/java/com/example/hmql_ebookapp/BookIntroductionActivity.kt
+++ app/src/main/java/com/example/hmql_ebookapp/BookIntroductionActivity.kt
@@ -7,22 +7,31 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
+@Suppress("DEPRECATION")
 class BookIntroductionActivity : AppCompatActivity() {
     private lateinit var sampleBookList : ArrayList<SampleBook>
     lateinit var bookNameList : Array<String>
     lateinit var authorNameList : Array<String>
     lateinit var bookImgIdList : Array<Int>
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_introduction)
         sampleDataInit()
+
+        if(savedInstanceState == null) { // initial transaction should be wrapped like this
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.readlikebtn_fragmentCV, BookIntroduction_ReadFavoriteFragment())
+                .commitAllowingStateLoss()
+        }
 
         val TagsRV = findViewById<RecyclerView>(R.id.TagsRV)
         val ChaptersRV = findViewById<RecyclerView>(R.id.ChaptersRV)
@@ -66,11 +75,7 @@ class BookIntroductionActivity : AppCompatActivity() {
             Toast.makeText(this, "See More Book Button Clicked", Toast.LENGTH_SHORT).show()
         }
 
-        supportFragmentManager.commit {
-            add<HomeFragment>(R.id.fragment_book_intro_purchasebtn)
-//            setReorderingAllowed(true)
-//            addToBackStack("name") // name can be null
-        }
+
     }
 
     private fun sampleDataInit() {
