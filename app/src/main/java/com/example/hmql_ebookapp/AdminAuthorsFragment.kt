ebc00a6fe0eaf5dfec52842b7d3d2cc6956d74f3
tuanlam1610 <tuanlam16102002@gmail.com>
2023-05-15 20:55:28 +0700

    Crud for admin to manage author info

--- app/src/main/java/com/example/hmql_ebookapp/AdminAuthorsFragment.kt
+++ app/src/main/java/com/example/hmql_ebookapp/AdminAuthorsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.hmql_ebookapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,7 @@ import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,14 +30,11 @@ class AdminAuthorsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private lateinit var listOfAuthor : ArrayList<Author>
     lateinit var authorNameList : Array<String>
     lateinit var authorImgIdList : Array<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sampleDataInit()
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -58,52 +57,74 @@ class AdminAuthorsFragment : Fragment() {
             requireActivity().supportFragmentManager.popBackStack()
         }
 
-        val authorsRv = view.findViewById<RecyclerView>(R.id.AdminAuthorRecyclerView)
-        val authorsRvAdapter = AdminAuthorsRecyclerViewAdapter(listOfAuthor)
-        authorsRv.layoutManager = LinearLayoutManager(requireContext())
-        authorsRv.adapter = authorsRvAdapter
-        authorsRvAdapter.onItemClick = { author ->
-            Toast.makeText(requireContext(), author.name.toString(), Toast.LENGTH_SHORT).show()
-            val bundle = Bundle()
-            bundle.putSerializable("authorDetail", author)
-            bundle.putInt("authorIndex", listOfAuthor.indexOf(author))
-            val authorDetailFragment = AdminAuthorDetailFragment()
-            authorDetailFragment.arguments = bundle
-            requireActivity().supportFragmentManager.commit {
-                replace(R.id.fragmentContainerView2, authorDetailFragment)
-                setReorderingAllowed(true)
-                addToBackStack("adminBookDetail")
+        val listOfAuthor = ArrayList<Author>()
+
+        val ref : DatabaseReference = FirebaseDatabase.getInstance().getReference("author")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (child in snapshot.children) {
+                        val author = child.getValue(Author::class.java)
+                        author?.let { listOfAuthor.add(it) }
+                    }
+                    val authorsRv = view.findViewById<RecyclerView>(R.id.AdminAuthorRecyclerView)
+                    val authorsRvAdapter = AdminAuthorsRecyclerViewAdapter(listOfAuthor)
+                    authorsRv.layoutManager = LinearLayoutManager(requireContext())
+                    authorsRv.adapter = authorsRvAdapter
+                    authorsRvAdapter.onItemClick = { author ->
+                        Toast.makeText(requireContext(), author.name.toString(), Toast.LENGTH_SHORT).show()
+                        val bundle = Bundle()
+                        bundle.putSerializable("authorDetail", author)
+                        bundle.putInt("authorIndex", listOfAuthor.indexOf(author))
+                        val authorDetailFragment = AdminAuthorDetailFragment()
+                        authorDetailFragment.arguments = bundle
+                        requireActivity().supportFragmentManager.commit {
+                            replace(R.id.fragmentContainerView2, authorDetailFragment)
+                            setReorderingAllowed(true)
+                            addToBackStack("adminBookDetail")
+                        }
+                    }
+
+                    setFragmentResultListener("editAuthor") { key, bundle ->
+                        val newName = bundle.getString("newName")
+                        val newDesc = bundle.getString("newDesc")
+                        val newImg = bundle.getInt("newImg")
+                        val authorIndex = bundle.getInt("authorIndex")
+                        listOfAuthor[authorIndex].name = newName.toString()
+                        listOfAuthor[authorIndex].description = newDesc.toString()
+//                        listOfAuthor[authorIndex].img = newImg.toString()
+                        val refTemp = FirebaseDatabase.getInstance().getReference("author/${listOfAuthor[authorIndex].authorID}");
+                        refTemp.setValue(listOfAuthor[authorIndex!!])
+                        authorsRvAdapter.notifyDataSetChanged()
+                    }
+
+                    val addBtn = view.findViewById<FloatingActionButton>(R.id.AdminAuthorAddBtn)
+                    addBtn.setOnClickListener(){
+                        requireActivity().supportFragmentManager.commit {
+                            replace<AdminAuthorAddFragment>(R.id.fragmentContainerView2)
+                            setReorderingAllowed(true)
+                            addToBackStack("adminAuthorAdd")
+                        }
+                    }
+
+                    setFragmentResultListener("addAuthor") { key, bundle ->
+                        val name = bundle.getString("name")
+                        val desc = bundle.getString("desc")
+                        val img = bundle.getInt("img")
+                        val refTemp = FirebaseDatabase.getInstance().getReference("author");
+                        val newChild = refTemp.push()
+                        val newAuthor = newChild.key?.let { Author(name!!, it, desc!!, "",ArrayList<String>()) }
+                        newChild.setValue(newAuthor);
+                        listOfAuthor.add(newAuthor!!)
+                        authorsRvAdapter.notifyDataSetChanged()
+                    }
+                }
             }
-        }
 
-        setFragmentResultListener("editAuthor") { key, bundle ->
-            val newName = bundle.getString("newName")
-            val newDesc = bundle.getString("newDesc")
-            val newImg = bundle.getInt("newImg")
-            val authorIndex = bundle.getInt("authorIndex")
-            listOfAuthor[authorIndex].name = newName.toString()
-            listOfAuthor[authorIndex].description = newDesc.toString()
-            listOfAuthor[authorIndex].img = newImg.toString()
-            authorsRvAdapter.notifyDataSetChanged()
-        }
-
-        val addBtn = view.findViewById<FloatingActionButton>(R.id.AdminAuthorAddBtn)
-        addBtn.setOnClickListener(){
-            requireActivity().supportFragmentManager.commit {
-                replace<AdminAuthorAddFragment>(R.id.fragmentContainerView2)
-                setReorderingAllowed(true)
-                addToBackStack("adminAuthorAdd")
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Error", "Failed to read value.", error.toException())
             }
-        }
-
-        setFragmentResultListener("addAuthor") { key, bundle ->
-            val name = bundle.getString("name")
-            val desc = bundle.getString("desc")
-            val img = bundle.getInt("img")
-            val newAuthor = Author(name.toString(), desc.toString(), img.toString())
-            listOfAuthor.add(newAuthor)
-            authorsRvAdapter.notifyDataSetChanged()
-        }
+        })
     }
 
     companion object {
@@ -124,47 +145,5 @@ class AdminAuthorsFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    private fun sampleDataInit() {
-        listOfAuthor = arrayListOf<Author>()
-
-        authorNameList = arrayOf(
-            "Alice Schertle 1",
-            "Alice Schertle 2",
-            "Jill McElmurry 1",
-            "Bret Bais 1",
-            "Liana Moriatory 1",
-            "Jill McElmurry 1",
-            "Alice Schertle 3",
-            "Jill McElmurry 3",
-            "Bret Bais 2",
-            "Liana Moriatory 5"
-        )
-
-        authorImgIdList = arrayOf(
-            R.drawable.sampleauthor1,
-            R.drawable.sampleauthor2,
-            R.drawable.sampleauthor3,
-            R.drawable.sampleauthor1,
-            R.drawable.sampleauthor2,
-            R.drawable.sampleauthor3,
-            R.drawable.sampleauthor1,
-            R.drawable.sampleauthor2,
-            R.drawable.sampleauthor3,
-            R.drawable.sampleauthor1
-        )
-
-        var sampleDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nulla pharetra, s" +
-                "celerisque turpis ac, elementum ipsum. Sed vel bibendum est. Nunc pulvinar tellus ut libero fringilla, " +
-                "finibus feugiat dui bibendum. Donec pellentesque nisl velit, vitae tincidunt risus posuere nec. " +
-                "Praesent non mauris vitae orci sollicitudin congue quis quis ex. Sed non nunc sit amet risus molestie porta eget a tortor. " +
-                "Cras non justo vel ex fringilla pharetra id ac neque. Etiam magna tellus, convallis vel orci aliquam, commodo malesuada tortor. " +
-                "Cras eget lorem sed orci dignissim lobortis. Aliquam erat volutpat. Donec eu felis scelerisque, gravida mauris non, rhoncus arcu."
-
-        for(i in authorNameList.indices) {
-            val author = Author(authorNameList[i], sampleDesc, authorImgIdList[i].toString())
-            listOfAuthor.add(author)
-        }
     }
 }
